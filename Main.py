3b8a60ac8338393b765362047fb97a07a257cc81
raziqyasak <raziqyasak10@gmail.com>
2025-11-03 02:15:19 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -2,7 +2,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import os
-
+ 
 # -----------------------------------
 # 🧬 Genetic Algorithm Simulation
 # -----------------------------------
