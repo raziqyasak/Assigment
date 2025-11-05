df6e0b1452c47030d0b9ef2a9c320c830686385a
raziqyasak <raziqyasak10@gmail.com>
2025-11-05 10:30:21 +0800

    Update Main.py

--- Main.py
+++ Main.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 import requests
 from io import StringIO
-
+ 
 # -----------------------------------
 #  Genetic Algorithm Simulation
 # -----------------------------------
